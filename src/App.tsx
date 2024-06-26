6fbc31845fc2814bdf0a8420205462a9c43ff988
Nankun-Xu <113734143+Nankun-Xu@users.noreply.github.com>
2024-06-25 23:50:47 -0700

    init and create homepage

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
